156c0eee1c726e4b124af16a3064618e1ed15e4a
omarjeina <work@omarjeina.com>
2023-10-23 14:27:55 +0200

    v1.0

--- index.js
+++ index.js
@@ -1,35 +1,76 @@
 const timeEl = document.querySelector("#time");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+const endButton = document.querySelector("#end");
 
-let pomodoro = .25 * 60;
-let short = 5 * 60;
-let long = 15 * 60;
 
-// while(pomodoro > 0){
-//     setInterval(() => { 
-//         countdown(pomodoro);
-//         pomodoro--;
-//        }, 1000);
-// }
+const pomodoroTime = .25 * 60;
+const shortTime = 5 * 60;
+const longTime = 15 * 60;
 
-const countdownTimer = setInterval(() => { 
-    countdown(pomodoro);
-    if(pomodoro == -1){
-        clearInterval(countdownTimer);
-        pomodoro = .25 * 60;
+let pomodoro = pomodoroTime;
+let short = shortTime;
+let long = longTime;
+
+timeEl.textContent = `00:15`;
+
+startButton.addEventListener('click', () => {
+
+    startButton.style.display = "none";
+    pauseButton.style.display = "block";
+    endButton.style.display = "block";
+
+    const countdownTimer = setInterval(() => { 
+
+        function endTimer(){
+            clearInterval(countdownTimer);
+            pomodoro = pomodoroTime;
+            countdown(pomodoro);
+        
+        
+            startButton.style.display = "block";
+            pauseButton.style.display = "none";
+            endButton.style.display = "none";
+        }
+
+        pomodoro--;
         countdown(pomodoro);
+
+        if(pomodoro == -1){
+            endTimer();
+        }
+
+        endButton.addEventListener('click', () => {
+            endTimer();
+        });
+
+        }, 1000);
+    
+    function countdown(duration){
+    
+        let minutes = Math.floor(duration / 60);
+        let seconds = duration % 60;
+    
+        minutes = minutes < 10 ? `0${minutes}` : minutes;
+        seconds = seconds < 10 ? `0${seconds}` : seconds;
+    
+        timeEl.textContent = `${minutes}:${seconds}`;
+    
     }
-    pomodoro--;
-    }, 1000);
+});
 
-function countdown(duration){
 
-    let minutes = Math.floor(duration / 60);
-    let seconds = duration % 60;
 
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
-    seconds = seconds < 10 ? `0${seconds}` : seconds;
 
-    timeEl.textContent = `${minutes}:${seconds}`;
+function endTimer(){
+    clearInterval(countdownTimer);
+    pomodoro = pomodoroTime;
+    countdown(pomodoro);
 
+
+    startButton.style.display = "block";
+    pauseButton.style.display = "none";
+    endButton.style.display = "none";
 }
+
 
